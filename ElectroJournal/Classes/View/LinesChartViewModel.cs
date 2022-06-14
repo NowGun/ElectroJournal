0fd7468e3e32bbcdf20d4b5039ba677fe35e5663
Daniil Zhirov <nowgun98@gmail.com>
2022-06-14 19:48:16 +0300

    Подгрузка графиков в админе групп

--- ElectroJournal/Classes/View/LinesChartViewModel.cs
+++ ElectroJournal/Classes/View/LinesChartViewModel.cs
@@ -3,14 +3,18 @@ using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
+using Microsoft.EntityFrameworkCore;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace ElectroJournal.Classes.View
 {
@@ -34,34 +38,70 @@ namespace ElectroJournal.Classes.View
         }
 
         public ObservableCollection<ISeries> Series { get; set; }
-        private void FillDates()
+        private async void FillDates()
         {
-            DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(-7));
-
             using zhirovContext db = new();
 
-            var g =  db.Groups.Where(g => g.TeachersIdteachers == Properties.Settings.Default.UserID).FirstOrDefault();
+            /* await Task.Run(() =>
+             {
+                 for (int i = 1; i <= 7; i++)
+                 {
+                     DateOnly date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(i)));
+                     var g = db.Groups.Where(g => g.TeachersIdteachers == Properties.Settings.Default.UserID).FirstOrDefault();
 
-            var s = db.Presences
-                .Where(s => DateOnly.FromDateTime(s.PresenceDatetime) >= date
-                && s.Student.GroupsIdgroupsNavigation.GroupsNameAbbreviated == g.GroupsNameAbbreviated)
-                .GroupBy(s => new { DateOnly = DateOnly.FromDateTime(s.PresenceDatetime), s.Student.GroupsIdgroupsNavigation.GroupsNameAbbreviated })
-                .OrderBy(s => s.Key.DateOnly)
-                .Select(s => new
-                {
-                    date = s.Key,
-                    Count = s.Count()
-                });
+                     var ss = db.Presences
+                 .Where(x => DateOnly.FromDateTime(x.PresenceDatetime) == date
+                  && x.Student.GroupsIdgroupsNavigation.GroupsNameAbbreviated == g.GroupsNameAbbreviated)
+                 .Select(x => x.StudentId)
+                 .Distinct()
+                 .Count();
 
+                     data.Add(new Data { Count = ss, Date = date });
+                 }
+             });
 
-            foreach (var p in s)
+             
+
+             db.Presences.Where(x => DateOnly.FromDateTime(x.PresenceDatetime) >= date).DistinctBy(x => x.StudentId)
+                 //.Select(x => x.StudentId) // DateOnly.FromDateTime(x.PresenceDatetime)
+                 .GroupBy(x => new Data { Date = DateOnly.FromDateTime(x.PresenceDatetime), Count = });
+
+             var hui = (from presences in db.Presences 
+                        where DateOnly.FromDateTime(presences.PresenceDatetime) >= date
+                         select db.Presences.First(x => x.StudentId)
+                        );*/
+
+            var date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(7)));
+
+            List<Data> data = db.RawSqlQuery<Data>(@$"select date(presence_datetime) as Date, count(distinct student_id) as Count
+                                                    from presences 
+                                                    group by date(presence_datetime)",
+                                                    (reader) =>
+                                                    {
+                                                        return new Data
+                                                        {
+                                                            Date = reader.GetFieldValue<DateOnly>("Date"),
+                                                            Count = reader.GetFieldValue<int>("Count")
+                                                        };
+                                                    });
+
+            var filteredData = data.Where(x => x.Date >= date);
+
+            foreach (var p in filteredData)
             {
-                var a = p.date;
-                var sdf = p.Count;
-                _observableValues.Add(new(sdf));
+                _observableValues.Add(new(p.Count));
             }
         }
-        public Axis[] XAxes { get; set; } =
+
+        class Data
+        {
+            public DateOnly Date { get; set; }
+            public int Count { get; set; }
+        };
+
+        
+
+            public Axis[] XAxes { get; set; } =
         {
             new Axis
             {
@@ -79,5 +119,29 @@ namespace ElectroJournal.Classes.View
     }
 
 
+    public static class Helper
+    {
+        public static List<T> RawSqlQuery<T>(this zhirovContext ctx, string query, Func<DbDataReader, T> map)
+        {
+            using (var command = ctx.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
 
+                ctx.Database.OpenConnection();
+
+                using (var result = command.ExecuteReader())
+                {
+                    var entities = new List<T>();
+
+                    while (result.Read())
+                    {
+                        entities.Add(map(result));
+                    }
+
+                    return entities;
+                }
+            }
+        }
+    }
 }
--- ElectroJournal/Classes/View/PieChartViewModel.cs
+++ ElectroJournal/Classes/View/PieChartViewModel.cs
@@ -10,6 +10,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace ElectroJournal.Classes.View;
 
@@ -21,56 +22,64 @@ public partial class PieChartViewModel
         FillPie();
     }
     public ObservableCollection<ISeries> Series { get; set; }
-    private void FillPie()
+    private async void FillPie()
     {
         if (Series.Count == 15) return;
         using zhirovContext db = new();
         DateOnly dt = DateOnly.FromDateTime(DateTime.Now);
-        var g = db.Groups.Where(g => g.TeachersIdteachers == Properties.Settings.Default.UserID).FirstOrDefault();
 
-        if (g != null)
+        await Task.Run(() =>
         {
-            /*            var j5 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "5").ToList();
-                        var j4 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "4").ToList();
-                        var j3 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "3").ToList();
-                        var j2 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "2").ToList();
-                        var jn = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "н").ToList();*/
-            var j5 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "5").ToList();
-            var j4 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "4").ToList();
-            var j3 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "3").ToList();
-            var j2 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "2").ToList();
-            var jn = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "н").ToList();
-            Series.Add(
-                    new PieSeries<ObservableValue>
-                    {
-                        Values = new[] { new ObservableValue(double.Parse(j5.Count.ToString())) },
-                        Name = "Отлично",
-                    });
-            Series.Add(
-                    new PieSeries<ObservableValue>
-                    {
-                        Values = new[] { new ObservableValue(double.Parse(j4.Count.ToString())) },
-                        Name = "Хорошо",
-                    });
-            Series.Add(
-                    new PieSeries<ObservableValue>
-                    {
-                        Values = new[] { new ObservableValue(double.Parse(j3.Count.ToString())) },
-                        Name = "Удовлетворительно",
-                    });
-            Series.Add(
-                    new PieSeries<ObservableValue>
-                    {
-                        Values = new[] { new ObservableValue(double.Parse(j2.Count.ToString())) },
-                        Name = "Неудовлетворительно",
-                    });
-            Series.Add(
-                    new PieSeries<ObservableValue>
-                    {
-                        Values = new[] { new ObservableValue(double.Parse(jn.Count.ToString())) },
-                        Name = "Отсутствия",
-                    });
-        }
+            var g = db.Groups.Where(g => g.TeachersIdteachers == Properties.Settings.Default.UserID).FirstOrDefault();
+
+            if (g != null)
+            {
+                /*            var j5 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "5").ToList();
+                            var j4 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "4").ToList();
+                            var j3 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "3").ToList();
+                            var j2 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "2").ToList();
+                            var jn = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == dt.Day.ToString() && j.JournalScore == "н").ToList();*/
+                var j5 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "5").ToList();
+                var j4 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "4").ToList();
+                var j3 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "3").ToList();
+                var j2 = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "2").ToList();
+                var jn = db.Journals.Where(j => j.StudentsIdstudentsNavigation.GroupsIdgroups == g.Idgroups && j.JournalDay == "16" && j.JournalScore == "н").ToList();
+
+                
+                    Series.Add(
+                        new PieSeries<ObservableValue>
+                        {
+                            Values = new[] { new ObservableValue(double.Parse(j5.Count.ToString())) },
+                            Name = "Отлично",
+                        });
+                Series.Add(
+                        new PieSeries<ObservableValue>
+                        {
+                            Values = new[] { new ObservableValue(double.Parse(j4.Count.ToString())) },
+                            Name = "Хорошо",
+                        });
+                Series.Add(
+                        new PieSeries<ObservableValue>
+                        {
+                            Values = new[] { new ObservableValue(double.Parse(j3.Count.ToString())) },
+                            Name = "Удовлетворительно",
+                        });
+                Series.Add(
+                        new PieSeries<ObservableValue>
+                        {
+                            Values = new[] { new ObservableValue(double.Parse(j2.Count.ToString())) },
+                            Name = "Неудовлетворительно",
+                        });
+                Series.Add(
+                        new PieSeries<ObservableValue>
+                        {
+                            Values = new[] { new ObservableValue(double.Parse(jn.Count.ToString())) },
+                            Name = "Отсутствия",
+                        });
+            }
+        });
+
+        
     }
 }
 
