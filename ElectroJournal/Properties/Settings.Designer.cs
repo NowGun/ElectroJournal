b28923f3a039423455a2010b62c04fb077cc41a9
Daniil Zhirov <nowgun98@gmail.com>
2022-06-19 22:50:41 +0300

    Обновление версии

--- ElectroJournal/Properties/Settings.Designer.cs
+++ ElectroJournal/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace ElectroJournal.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.3.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.4.0")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
